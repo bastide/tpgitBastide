1d7779b75f16ff9e3ea721754a2c86e037bbe6f3
Rémi Bastide <Remi.Bastide@irit.fr>
2024-10-16 09:13:23 +0200

    3° commit

--- src/main/java/org/github/bastide/Main.java
+++ src/main/java/org/github/bastide/Main.java
@@ -3,6 +3,7 @@ package org.github.bastide;
 public class Main {
     // commentaire
     // 2° commentaire
+    // 3° commentaire
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
