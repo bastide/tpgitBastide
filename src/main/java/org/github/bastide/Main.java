2001fd52c60fc71ab32cf48350c0800a2424b284
Rémi Bastide <Remi.Bastide@irit.fr>
2024-10-16 09:45:25 +0200

    modif vs-code

--- src/main/java/org/github/bastide/Main.java
+++ src/main/java/org/github/bastide/Main.java
@@ -4,6 +4,7 @@ public class Main {
     // commentaire
     // 2° commentaire
     // 3° commentaire
+    // commentaire sur vs-code
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
